a127b2bd1e5b6d06c9f611e99997c4393ac26ca2
Hasanain Mohammed <hasanain98@gmail.com>
2019-04-21 21:41:44 -0500

    Updates

---
+++ precache-manifest.7db00ce14794c65626b1f5e85b6a85b8.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = [
+  {
+    "revision": "3dcd74c7ea0f40efc51f",
+    "url": "/clicky-game/static/css/main.dc90e949.chunk.css"
+  },
+  {
+    "revision": "3dcd74c7ea0f40efc51f",
+    "url": "/clicky-game/static/js/main.1a84e335.chunk.js"
+  },
+  {
+    "revision": "265538ab0e4606d350e6",
+    "url": "/clicky-game/static/js/runtime~main.27874bd3.js"
+  },
+  {
+    "revision": "4743c039c04edea7e174",
+    "url": "/clicky-game/static/js/2.1db7aab6.chunk.js"
+  },
+  {
+    "revision": "45bcfa60cbce44e8493981fa134d0a41",
+    "url": "/clicky-game/static/media/avengers-theme.45bcfa60.png"
+  },
+  {
+    "revision": "ce1132f5f4f01cd9f27dff3d7dc2e928",
+    "url": "/clicky-game/index.html"
+  }
+];
